0df39df30ad7a424cf70ea3c3ca226260b036506
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2026-02-19 21:26:51 +0300

    123

--- test/edit_task_test.py
+++ test/edit_task_test.py
@@ -1,13 +1,11 @@
 import requests
 
 def test_edit():
+    """это тест про тест"""
     body = {"title":"generated","completed":False}
     response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
     id = response.json()["id"]
-    
-    body = {"title":"generated-1"}
-    response = requests.patch(f'https://todo-app-sky.herokuapp.com/{id}', json=body)
-    assert response.status_code == 200
+
     
     response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
     assert response.status_code == 200
